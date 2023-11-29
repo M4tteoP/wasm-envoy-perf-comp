7260d536753166b1104a436be08f256646e60161
Matteo Pace <pace.matteo96@gmail.com>
2023-11-29 15:56:45 +0100

    moves to k6

--- testCmds.go
+++ testCmds.go
@@ -5,6 +5,6 @@ package main
 var urlRequest = "http://localhost:8080/anything"
 
 var testParamsMap = map[string][]string{
-	"noop": {"run", "get-constant-arrival-rate.js", "--env", "URL=" + urlRequest}, // 10000 RPS - 20/40 concurrent requests
+	"noop": {"run", "./k6/get-constant-arrival-rate.js", "--env", "URL=" + urlRequest}, // 10000 RPS - 20/40 concurrent requests
 	// TODO: add the other tests and their params
 }
